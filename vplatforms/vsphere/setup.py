7c5e9b735e6ae1a1a7a2fc1ecee6fe32560b95f3
FreakyFreddie <FreakyFreddie@users.noreply.github.com>
2017-11-06 16:46:25 +0100

    removed setup from vsphere module
    dependencies will be installed with CTFd-setup bash script

--- vplatforms/vsphere/setup.py
+++ vplatforms/vsphere/setup.py
@@ -1,9 +1,11 @@
 # install module dependencies etc.
 def setup():
     import os
-    import pip
+    #import pip
 
-    virt_opt_dir = os.path.abspath(os.path.dirname(__file__))
+    #virt_opt_dir = os.path.abspath(os.path.dirname(__file__))
 
     # install requirements for module & submodules
-    pip.main(['install', '-r', virt_opt_dir + '/requirements.txt', '--extra-index-url', 'file://' + virt_opt_dir + '/lib'])
+    #still a problem: pip shouldn't be used in the same process
+    #pip.main(['install', '-r', virt_opt_dir + '/requirements.txt', '--extra-index-url', 'file://' + virt_opt_dir + '/lib'])
+    
