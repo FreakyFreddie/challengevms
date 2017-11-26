4c70f03140ebcf2048a6df358bea34bbb6caa730
FreakyFreddie <FreakyFreddie@users.noreply.github.com>
2017-11-26 12:35:17 +0100

    forgot manage/vm/

--- __init__.py
+++ __init__.py
@@ -113,31 +113,30 @@ def load(app):
         return json.dumps(vms)
     # Check if not in blacklist (after connecting to ...)
 
-    @vspherevms.route('/admin/challengeVMs/manage/vm/<string:vm_uuid>/poweron', methods=['GET', 'POST'])
+    @vspherevms.route('/admin/vspherevms/manage/vm/<string:vm_uuid>/poweron', methods=['GET', 'POST'])
     @admins_only
     def poweron_vm(vm_uuid):
         return powerstate_operation(vm_uuid, "powerOn")
 
-    @vspherevms.route('/admin/challengeVMs/manage/vm/<string:vm_uuid>/suspend', methods=['GET', 'POST'])
+    @vspherevms.route('/admin/vspherevms/manage/vm/<string:vm_uuid>/suspend', methods=['GET', 'POST'])
     @admins_only
     def suspend_vm(vm_uuid):
         return powerstate_operation(vm_uuid, "Suspend")
 
-    @vspherevms.route('/admin/challengeVMs/manage/vm/<string:vm_uuid>/shutdown', methods=['GET', 'POST'])
+    @vspherevms.route('/admin/vspherevms/manage/vm/<string:vm_uuid>/shutdown', methods=['GET', 'POST'])
     @admins_only
     def shutdown_vm(vm_uuid):
         return powerstate_operation(vm_uuid, "Shutdown")
 
-    @vspherevms.route('/admin/challengeVMs/manage/vm/<string:vm_uuid>/restart', methods=['GET', 'POST'])
+    @vspherevms.route('/admin/vspherevms/manage/vm/<string:vm_uuid>/restart', methods=['GET', 'POST'])
     @admins_only
     def restart_vm(vm_uuid):
         return powerstate_operation(vm_uuid, "Reboot")
 
-    @vspherevms.route('/admin/challengeVMs/manage/vm/<string:vm_uuid>/resume', methods=['GET', 'POST'])
+    @vspherevms.route('/admin/vspherevms/manage/vm/<string:vm_uuid>/resume', methods=['GET', 'POST'])
     @admins_only
     def resume_vm(vm_uuid):
         return powerstate_operation(vm_uuid, "Resume")
-
 
 
     def fetch_vm_by_uuid(vm_uuid, service_instance):
